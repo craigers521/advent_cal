24af46c3f542edf6a21917c4ead91d4250cccd18
craigers521 <craigers521@gmail.com>
2022-12-05 23:50:47 -0600

    day6

---
+++ day6/signal_tuner.py
@@ -0,0 +1,24 @@
+
+def read_input(filename):
+    with open(filename) as file:
+        contents = file.read()
+    return contents
+
+def find_buff_start(sig, offset):
+    idx = offset
+    while True:
+        temp_set = set(sig[idx-offset:idx])
+        if len(temp_set) == offset:
+            break
+        else:
+            idx += 1
+    return idx
+
+def main():
+    sig = read_input('input.txt')
+    offset = 14
+    start_idx = find_buff_start(sig, offset)
+    print(start_idx)
+
+if __name__ == "__main__":
+    main()
