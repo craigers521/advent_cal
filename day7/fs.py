2dd653593c971b58c20bb33854d9c8ceabf4ed40
craigers521 <craigers521@gmail.com>
2022-12-07 14:50:03 -0600

    this is still broken

--- day7/fs.py
+++ day7/fs.py
@@ -1,8 +1,9 @@
-import pydash
-import json
+#import pydash
+#import json
 
 fs = {}
 path = []
+dirsize = {}
 
 def read_input(filename):
     lines = []
@@ -27,29 +28,46 @@ def mkfs(sdout):
             if file[0] == "dir":
                 continue
             else:
-                item = {file[1]:int(file[0])}
-                update_fs(item)
+                #item = {file[1]:int(file[0])}
+                update_dirsize(int(file[0]))
+                #update_fs(item)
     pass
 
-def update_fs(item):
-    sep = "."
-    dp = sep.join(path)
-    old_dict = pydash.get(fs, dp)
-    temp_dict = {'temp_key': {}}
-    if old_dict is not None:
-        temp_dict = {'temp_key': old_dict }
-    temp_dict['temp_key'].update(item)
-    pydash.update(fs, dp, temp_dict['temp_key'])
+#def update_fs(item):
+#    #sep = "."
+#    #dp = sep.join(path)
+#    old_dict = pydash.get(fs, path)
+#    temp_dict = {'temp_key': {}}
+#    if old_dict is not None:
+#        temp_dict = {'temp_key': old_dict }
+#    temp_dict['temp_key'].update(item)
+#    pydash.update(fs, path, temp_dict['temp_key'])
+#    pass
+
+
+def update_dirsize(size):
+    for i in range(0,len(path)):
+        if path[i] in dirsize:
+            dirsize[path[i]] = dirsize[path[i]]+size
+        else:
+            dirsize[path[i]] = size
     pass
 
 
-            
+def sumdirs(maxsize):
+    dirsum = 0
+    for k in dirsize:
+        if dirsize[k] <= maxsize:
+            dirsum = dirsum+dirsize[k]
+    return dirsum
 
 
 def main():
     sdout = read_input('input.txt')
     mkfs(sdout)
-    print(json.dumps(fs, indent=4))
+    #print(json.dumps(fs, indent=4))
+    dirsum = sumdirs(100000)
+    print(dirsum)
 
 if __name__ == "__main__":
     main()
