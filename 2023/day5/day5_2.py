c4fc424e913a2d267013479d62b5da8252f7b595
craigers521 <craigers521@gmail.com>
2023-12-05 14:46:17 -0600

    fuck day5 part 2

--- 2023/day5/day5_2.py
+++ 2023/day5/day5_2.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+start = datetime.now()
+
 def read_input(filename):
     f = open(filename).read().strip()
     return f
@@ -13,25 +16,34 @@ def parse_seeds(lines):
 class Mapthing:
     def __init__(self,mappings):
         self.tups = [[int(x) for x in mapping.split()] for mapping in mappings]
+
     def run_one(self,input):
         for dst,src,rng in self.tups:
             src_end = src+rng
             if src <= input < src_end:
                 return dst+(input-src)
         return input
+
     def run_range(self,interval):
-        for dst,src,rng in self.tuples:
+        answers = []
+        for dst,src,rng in self.tups:
             src_end = src+rng
+            temp_interval = []
             while interval:
                 (start,end) = interval.pop()
                 before = (start,min(end,src)) #from seed start to lower of seed range end and source start
                 inter = (max(start,src), min(src_end, end)) #higher of seed start and source start to lower of source end and seed end
                 after = (max(src_end, start), end) #higher of seed start and source end to seed end
                 if before[1] > before[0]:
+                    temp_interval.append(before)
+                if inter[1] > inter[0]:
+                    answers.append((dst+(inter[0]-src), dst+(inter[1]-src)))
+                if after[1] > after[0]:
+                    temp_interval.append(after)
+            interval = temp_interval
+        return answers+interval
 
 
-
-        
 def find_locs(seeds, Mapthings):
     locs = []
     for seed in seeds:
@@ -41,11 +53,14 @@ def find_locs(seeds, Mapthings):
     return min(locs)
 
 def find_intervals(seedpairs, Mapthings):
+    locs = []
     for sseed,eseed in seedpairs:
         seedr = [(sseed, sseed+eseed)]
         for Mapthing in Mapthings:
             seedr = Mapthing.run_range(seedr)
-    
+        locs.append(min(seedr)[0])
+    return min(locs)
+
 def main():
     lines = read_input("input.txt")
     seeds,maps = parse_seeds(lines)
@@ -54,6 +69,10 @@ def main():
     print(p1)
     seedpairs = [seeds[i:i+2] for i in range(0,len(seeds),2)]
     p2 = find_intervals(seedpairs, Mapthings)
+    print(p2)
+    end = datetime.now()
+    td = (end - start).total_seconds() * 10**3
+    print(f"execution time: {td}ms")
 
 if __name__ == "__main__":
     main()
