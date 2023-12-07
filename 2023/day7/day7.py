589e765e913a4742e3d23b6df408d93c90f4beda
craigers521 <craigers521@gmail.com>
2023-12-07 17:58:56 -0600

    refactoring day 7 part 1, too lazy for pt2

--- 2023/day7/day7.py
+++ 2023/day7/day7.py
@@ -1,4 +1,4 @@
-from collections import defaultdict 
+from collections import defaultdict, Counter
 
 class Hand:
     card_order = {"2": 1, "3": 2, "4": 3, "5": 4, "6": 5, "7": 6, "8": 7, "9": 8, "T": 9, "J": 10, "Q": 11, "K": 12, "A": 13}
@@ -6,94 +6,74 @@ class Hand:
     def __init__(self,card_tup):
         self.hand = card_tup[0]
         self.bid = card_tup[1]
+        self.card_counts = Counter(self.hand)
         self.rank = self.find_rank()
+        self.hand = self.hand.replace('T',chr(ord('9')+1))
+        self.hand = self.hand.replace('J',chr(ord('9')+2))
+        self.hand = self.hand.replace('Q',chr(ord('9')+3))
+        self.hand = self.hand.replace('K',chr(ord('9')+4))
+        self.hand = self.hand.replace('A',chr(ord('9')+5))
     
     def __repr__(self):
         rep = f"Hand: {self.hand} Bid: {self.bid} Rank: {self.rank}"
         return rep
     
     def find_rank(self):
-        score = self.base_score()
+        score = 1
         if self.check_five(): 
-            score += 1200000000
+            score = 9
             return score
         if self.check_four(): 
-            score += 1000000000
+            score = 8
             return score
         if self.check_house(): 
-            score += 800000000
+            score = 7
             return score
         if self.check_three(): 
-            score += 600000000
+            score = 6
             return score
         if self.check_two(): 
-            score += 400000000
+            score = 5
             return score
         if self.check_one(): 
-            score += 200000000
+            score = 4
             return score
         return score
     
     def check_five(self):
-        card_counts = defaultdict(int)
-        for card in self.hand:
-            card_counts[card] += 1
-        if sorted(card_counts.values()) == [5]:
+        if sorted(self.card_counts.values()) == [5]:
             return True
         return False
     
     def check_four(self):
-        card_counts = defaultdict(int)
-        for card in self.hand:
-            card_counts[card] += 1
-        if sorted(card_counts.values()) == [1,4]:
+        if sorted(self.card_counts.values()) == [1,4]:
             return True
         else: 
             return False
 
     def check_house(self):
-        card_counts = defaultdict(int)
-        for card in self.hand:
-            card_counts[card] += 1
-        if sorted(card_counts.values()) == [2,3]:
+        if sorted(self.card_counts.values()) == [2,3]:
             return True
         else:
             return False
 
     def check_three(self):
-        card_counts = defaultdict(int)
-        for card in self.hand:
-            card_counts[card] += 1
-        if set(card_counts.values()) == set([3,1]):
+        if set(self.card_counts.values()) == set([3,1]):
             return True
         else:
             return False
 
     def check_two(self):
-        card_counts = defaultdict(int)
-        for card in self.hand:
-            card_counts[card] += 1
-        if sorted(card_counts.values()) == [1,2,2]:
+        if sorted(self.card_counts.values()) == [1,2,2]:
             return True
         else:
             return False
 
     def check_one(self):
-        card_counts = defaultdict(int)
-        for card in self.hand:
-            card_counts[card] += 1
-        if 2 in card_counts.values():
+        if 2 in self.card_counts.values():
             return True
         else:
             return False
-
-    def base_score(self):
-        x=4
-        value = 0
-        for card in self.hand:
-            value += self.card_order[card]*50**x
-            x -= 1
-        return value
 
 
 def read_input(filename):
@@ -114,7 +94,7 @@ def card_sort(hands):
     for i in range(1,len(hands)):
         key = hands[i]
         j = i-1
-        while j >= 0 and key.rank < hands[j].rank:
+        while j >= 0 and (key.rank, key.hand) < (hands[j].rank, hands[j].hand):
             hands[j+1] = hands[j]
             j -= 1
         hands[j+1] = key
@@ -127,13 +107,9 @@ def find_winnings(hands):
     return winnings
 
 
-
-
 def main():
     lines = read_input("input.txt")
     hands = make_hands(lines)
-    #for hand in hands: print(repr(hand))
-    #print('\n')
     sorted_hands = card_sort(hands)
     #for hand in sorted_hands: print(repr(hand))
     p1 = find_winnings(sorted_hands)
